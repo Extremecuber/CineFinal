8535582c84de7320efa2b348adc4cb85a520bf1f
ExtremeCuber <vikyath.danamaraju@gmail.com>
2024-06-09 21:05:15 +0530

    no errors secret access key

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   accessKeyId: process.env.AKIA2W6XGUVKFAULV6UF,
-  secretAccessKey: process.env.xaupXaVACoWpICLS8u4RUY7+mFtppcbLNnM8rX4B,
+  secretAccessKey: process.env.xaupXaVACoWpICLS8u4RUY7mFtppcbLNnM8rX4B,
   region: process.env.eu-north-1, // e.g., 'us-east-1'
   bucketName: process.env.cinedleframes,
 };
