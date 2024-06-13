077993dd3954e3aa739b99fa51d889a9a1441624
ExtremeCuber <vikyath.danamaraju@gmail.com>
2024-06-13 17:34:44 +0530

    V9

--- public/script.js
+++ public/script.js
@@ -134,7 +134,7 @@ async function loadImagesFromS3() {
                     console.log('Fetched images from selected folder:', imagesData); // Debugging step
 
                     if (imagesData && imagesData.frames.length > 0) {
-                        frames.push(...imagesData.frames.slice(1,7));
+                        frames.push(...imagesData.frames.slice(0,6));
 
                         // Set the correct movie title
                         correctMovie = randomFolder; // Assuming the folder name is the movie title
